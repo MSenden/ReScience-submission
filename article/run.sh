a4fcda1ac76cbe9b705239472f9801c8c3063a87
MSenden <mario.senden@googlemail.com>
2017-03-09 16:37:17 +0100

    replaced pandoc-crossref filter with fignos tablenos and eqnos

--- article/run.sh
+++ article/run.sh
@@ -1,6 +1,4 @@
-#pandoc --standalone --filter ~/.cabal/bin/pandoc-crossref --template=rescience-template.tex --latex-engine=xelatex --biblatex --bibliography=senden-schuecker-hahne-diesmann-goebel-2017.bib -M "crossrefYaml=crossref.yaml" --output senden-schuecker-hahne-diesmann-goebel-2017.tex senden-schuecker-hahne-diesmann-goebel-2017.md
-
-pandoc --standalone --template=rescience-template.tex --latex-engine=xelatex --biblatex --bibliography=senden-schuecker-hahne-diesmann-goebel-2017.bib -M "crossrefYaml=crossref.yaml" --output senden-schuecker-hahne-diesmann-goebel-2017.tex senden-schuecker-hahne-diesmann-goebel-2017.md
+pandoc --standalone --filter pandoc-fignos --filter pandoc-tablenos --filter pandoc-eqnos --template=rescience-template.tex --latex-engine=xelatex --biblatex --bibliography=senden-schuecker-hahne-diesmann-goebel-2017.bib -M "crossrefYaml=crossref.yaml" --output senden-schuecker-hahne-diesmann-goebel-2017.tex senden-schuecker-hahne-diesmann-goebel-2017.md
 
 xelatex senden-schuecker-hahne-diesmann-goebel-2017
 biber senden-schuecker-hahne-diesmann-goebel-2017
